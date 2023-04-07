687521b9d73f1e8cdb9544fde9d753d5ff5c313e
juanedcabrera <juanedcabrera@gmail.com>
2023-04-07 17:14:46 -0600

    Entry pseudocode complete

---
+++ controllers/entry.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// GET /entries -- INDEX route to show all the entries
+
+// GET /entries/new -- SHOW form to create a new entry
+
+// POST /entries -- CREATE route to add a new entry
+
+// GET /entries/:id -- SHOW route to display a single entry
+
+// GET /entries/:id/edit -- SHOW form to edit an entry
+
+// PUT /entries/:id -- UPDATE route to modify an entry

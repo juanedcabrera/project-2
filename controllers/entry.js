7977345c01ff4e37d409e232d3cdb86d88627455
juanedcabrera <juanedcabrera@gmail.com>
2023-04-07 18:25:00 -0600

    GET route to add new entry

--- controllers/entry.js
+++ controllers/entry.js
@@ -2,13 +2,23 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-// GET /entries -- INDEX route to show all the entries
-router.get('/', async (req, res) => {
-    res.send('hello everyone')
-})
-// GET /entries/new -- SHOW form to create a new entry
 
+// GET /entries/new -- SHOW form to create a new entry
+router.get('/new', (req, res) => {
+    res.render('entries/new.ejs')
+})
 // POST /entries -- CREATE route to add a new entry
+
+// GET /entries -- INDEX route to show all the entries
+// router.get('/', async (req, res) => {
+//     try {
+//         const entries = await db.entry.findAll()
+//         res.render('entries/index.ejs', { entries})
+//     } catch (err) {
+//         console.log(err)
+//         res.redirect('/')
+//     }
+// })
 
 // GET /entries/:id -- SHOW route to display a single entry
 

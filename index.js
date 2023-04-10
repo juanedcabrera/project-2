c1a548543657d632fa04af2efdf6d23149a2f035
juanedcabrera <juanedcabrera@gmail.com>
2023-04-10 15:34:53 -0600

    Added CSS

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const methodOverride = require('method-override');
 const app = express()
 const PORT = process.env.PORT || 8000
 app.set('view engine', 'ejs')
-
+app.use(express.static('public/'))
 app.use(methodOverride('_method'))
 // middlewares
 // parse html from request bodies

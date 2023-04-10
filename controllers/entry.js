158b11110c4b13a2856ba5613d4a2257ad5655e5
juanedcabrera <juanedcabrera@gmail.com>
2023-04-09 18:17:52 -0600

    Edit entries works

--- controllers/entry.js
+++ controllers/entry.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const cryptoJs = require('crypto-js')
+const cryptoJs = require("crypto-js");
 
 // GET /entries/new -- SHOW form to create a new entry
-router.get('/new', async (req, res) => {
-  let quotes = []
-  const apiUrl = "https://api.themotivate365.com/stoic-quote"
+router.get("/new", async (req, res) => {
+  let quotes = [];
+  const apiUrl = "https://api.themotivate365.com/stoic-quote";
   try {
     await fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => {
-      quotes = data
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        quotes = data;
+      });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 
   const user = res.locals.user;
-  res.render('entries/new.ejs', { 
+  res.render("entries/new.ejs", {
     user,
-    quotes
-   });
+    quotes,
   });
-
-// router.get('/unauthorized') method
-router.get('/unauthorized', function(req, res, next) {
-  res.render('unauthorized', { user: req.user, partials: { header: './partials/header' } });
 });
 
+// router.get('/unauthorized') method
+router.get("/unauthorized", function (req, res, next) {
+  res.render("unauthorized", {
+    user: req.user,
+    partials: { header: "./partials/header" },
+  });
+});
 
 // POST /entries -- CREATE route to add a new entry
 router.post("/", async (req, res) => {
@@ -50,89 +52,92 @@ router.post("/", async (req, res) => {
       userId: user.id,
     });
 
-// tag logic
-let foundTags = []
-// convert to an array with a single element
-if (!Array.isArray(tags)) {
-  tags = [tags] 
-}
-for (let tag of tags) {
-  let foundTag = await db.tag.findOne({
-    where: {
-      name: tag
+    // tag logic
+    let foundTags = [];
+    // convert to an array with a single element
+    if (!Array.isArray(tags)) {
+      tags = [tags];
     }
-  })
-  foundTags.push(foundTag)
-}
+    for (let tag of tags) {
+      let foundTag = await db.tag.findOne({
+        where: {
+          name: tag,
+        },
+      });
+      foundTags.push(foundTag);
+    }
 
-newEntry.addTags(foundTags)
-
-
-    // if (typeof tags === 'array') {
-    //   const tagEntries = await db.tag.bulkCreate(
-    //     tags.map(tag => ({ name: tag}))
-
-    //   )
-    // } else {
-    //   const tagEntry = await db.tag.create({
-    //     name:tags
-    //   })
-    // }
-
+    newEntry.addTags(foundTags);
 
     res.redirect("/entries");
   } catch (err) {
     console.log(err);
     res.redirect("/");
   } finally {
-    console.log('hello')
+    console.log("hello");
   }
 });
 
-
-
-
 // GET /entries -- INDEX route to show all the entries
-router.get('/', async (req, res) => {
-  console.log(`user is ${res.locals.user.email} `)
-    try {
-        const entries = await db.entry.findAll({
-          where: {
-            userId: res.locals.user.id
-        }
-      })
-        res.render('entries/index.ejs', { entries})
-    } catch (err) {
-        console.log(err)
-        res.redirect('/')
-    }
-})
+router.get("/", async (req, res) => {
+  console.log(`user is ${res.locals.user.email} `);
+  try {
+    const entries = await db.entry.findAll({
+      where: {
+        userId: res.locals.user.id,
+      },
+    });
+    res.render("entries/index.ejs", { entries });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
 
 // GET /entries/:id -- SHOW route to display a single entry
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const foundEntry = await db.entry.findAll({
       where: {
         // filter entry by current user logged in
         userId: res.locals.user.id,
-        id: req.params.id
-      }
-    })
-    if(!foundEntry.length) {
+        id: req.params.id,
+      },
+    });
+    if (!foundEntry.length) {
       // Entry not found redirect to index
-      return res.redirect('/entries?message=Entry not found')
+      return res.redirect("/entries?message=Entry not found");
     } else {
       // render show page for entry
-      res.render('entries/show.ejs', {
-        entry: foundEntry[0]
-      })}
+      res.render("entries/show.ejs", {
+        entry: foundEntry[0],
+      });
+    }
   } catch (err) {
-    console.log(err)
-    res.redirect('/entries?message=An error occured')
+    console.log(err);
+    res.redirect("/entries?message=An error occured");
   }
-})
+});
 
 // GET /entries/:id/edit -- SHOW form to edit an entry
+router.get("/:id/edit", async (req, res) => {
+  try {
+    // Find the entry with the given ID
+    const foundEntry = await db.entry.findAll({
+      where: {
+        // filter entry by current user logged in
+        userId: res.locals.user.id,
+        id: req.params.id,
+      },
+    });
+
+    // Render the edit form with the entry data
+    res.render("entries/edit-entry", { entry: foundEntry[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // PUT /entries/:id -- UPDATE route to modify an entry
 
